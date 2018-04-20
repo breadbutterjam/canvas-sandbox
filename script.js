edcfbfbe9fa7c156b3fe0834ad67f6007a38766c
breadbutterjam <jaimeen@gmail.com>
2018-04-21 01:44:19 +0530

    draw a triangle

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let DECIMAL_PLACES = 5;
+let INNER_TRIANGLE_SIDE = 100;
+let TRIANGLE_ANGLE = Math.PI / 3;
+
 
 function Point(x,y)
 {
@@ -29,13 +32,17 @@ let contex;
 //points declaration
 let ptA; 
 let ptB;
-
+let ptC;
 
 //equation for line AB
 let eqnAB;
 
+let innerTriHt;
+
 function onBodyLoad()
 {
+    innerTriHt = roundToDecimalPlaces(Math.sin(TRIANGLE_ANGLE) * INNER_TRIANGLE_SIDE, 2);
+
     //alert ("A");
     canvasElem = document.getElementById("myCanvas");
     contex = canvasElem.getContext("2d");
@@ -60,8 +67,14 @@ function canvasClicked(event)
 
     eqnAB = new EquationOfLine(m, c);
 
-    plotDot(ptA);
+    // plotDot(ptA);
 
+
+    getPtB();
+    getPtC();
+    DrawLineAB();
+    DrawLineBC();
+    DrawLineCA();
 }
     
 
@@ -96,4 +109,48 @@ function clearCanvas()
 {
     contex.clearRect(0,0,canvasElem.width, canvasElem.height);
     contex.moveTo(0, 0);
+}
+
+
+function getPtC()
+{
+    let ptC_y = ptA.y + innerTriHt;
+    let ptC_x = ptB.x + INNER_TRIANGLE_SIDE;
+
+    ptC = new Point(ptC_x, ptC_y);
+}
+
+function getPtB()
+{
+    let ptB_y = ptA.y + innerTriHt;
+    let ptB_x;
+
+    let sideSquare = Math.pow(INNER_TRIANGLE_SIDE, 2);
+    let htSquare = Math.pow(innerTriHt, 2);
+    // ptB_x = Math.sqrt(sideSquare - htSquare) + ptA.x;
+    ptB_x =  ptA.x - Math.sqrt(sideSquare - htSquare) ;
+    ptB = new Point(ptB_x, ptB_y);
+
+}
+
+function DrawLineAB()
+{
+    DrawLineFromTo(ptA, ptB);
+}
+
+function DrawLineBC()
+{
+    DrawLineFromTo(ptB, ptC);
+}
+
+function DrawLineCA()
+{
+    DrawLineFromTo(ptC, ptA);
+}
+
+function DrawLineFromTo(fromPt, toPt)
+{
+    contex.moveTo(fromPt.x, fromPt.y);
+    contex.lineTo(toPt.x, toPt.y);
+    contex.stroke();
 }
